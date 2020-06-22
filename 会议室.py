677f2ab5a701d61bfb012e7798c3b7f76cd751c7
Yufeng-L <yflin@bu.edu>
2020-06-22 01:04:11 -0400

    增加最小堆解法

--- 会议室.py
+++ 会议室.py
@@ -16,6 +16,22 @@ class Solution:
                 rooms.append(meeting[1])
         return len(rooms)
 
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if len(intervals) == 0:
+            return 0
+        # 根据会议开始时间排序
+        intervals.sort()
+        rooms = [intervals[0][0]]   # 房间内保存最早结束时间
+        for start, end in intervals:
+            if rooms[0] <= start:       # 有空余房间
+                heapq.heapreplace(rooms, end)
+            else:                   # 没有空余房间
+                heapq.heappush(rooms, end)
+        return len(rooms)           # 返回房间个数
+
 if __name__ == "__main__":
     test = [[0,30],[5,10],[15,20],[17,25],[19,24],[35,50],[49,61],[62,63]]
     print(sorted(test))
