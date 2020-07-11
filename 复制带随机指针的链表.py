e0d9c1ac1dc4404e6cceb8c37499979061af62c7
Yufeng-L <yflin@bu.edu>
2020-07-10 23:45:15 -0400

    lc138

---
+++ 复制带随机指针的链表.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        def dfs(head):
+            if not head: return 
+            if head in visited:
+                return visited[head]
+            copy = Node(head.val, None, None)
+            visited[head] = copy
+            copy.next = dfs(head.next)
+            copy.random = dfs(head.random)
+            return copy
+        visited = dict()
+        return dfs(head)
