edd70a7ed5f5215f5d3d929e3232425b090b40cd
Yufeng-L <yflin@bu.edu>
2020-07-25 04:01:00 -0400

    update

--- sorting.py
+++ sorting.py
@@ -11,12 +11,12 @@ def bubble(arr):
 #Insertion Sort 插入排序(稳定)
 def insertion(arr):
 	for i in range(1,len(arr)):
-		key_item = arr[i]
+		key = arr[i]
 		j = i - 1
-		while j >= 0 and arr[j] > key_item:
+		while j >= 0 and arr[j] > key:
 			arr[j+1] = arr[j]
 			j -= 1
-		arr[j+1] = key_item
+		arr[j+1] = key
 	return arr
 
 #Merge Sort 合并排序(稳定)
