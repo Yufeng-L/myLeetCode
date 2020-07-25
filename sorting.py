9119e2ea544675d6cd094ccb77ee4471650a3c26
Yufeng-L <yflin@bu.edu>
2020-07-25 04:09:59 -0400

    update

--- sorting.py
+++ sorting.py
@@ -13,7 +13,7 @@ def insertion(arr):
 	for i in range(1,len(arr)):
 		key = arr[i]
 		j = i - 1
-		while j >= 0 and arr[j] > key:
+		while j >= 0 and key < arr[j]:
 			arr[j+1] = arr[j]
 			j -= 1
 		#到这一步如果进了while后 j肯定为-1 arr[j+1]实为a[0] 
